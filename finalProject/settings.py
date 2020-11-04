61862b1b3c7389bbfc995935333493280ae7289f
Rustam Garaev <garaev.rustam.h@gmail.com>
2020-11-04 06:22:20 +0400

    fix debug setting

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.getenv('DEBUG'))
+DEBUG = int(os.getenv('DEBUG', default=False))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
