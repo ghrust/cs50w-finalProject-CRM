c68a3aa69e60af9b4b90a1bbc372f614c486b3bc
Rustam Garaev <garaev.rustam.h@gmail.com>
2020-11-25 21:07:59 +0400

    add CustomerSerializer

--- crm/serializers.py
+++ crm/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework import fields
 from .models import User, Customer, Company
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,3 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'date_joined']
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Customer model.
+    """
+    class Meta:
+        model = Customer
+        fields = '__all__'
