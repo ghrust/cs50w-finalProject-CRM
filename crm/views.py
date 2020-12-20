2aa695b2d83f00f7a244245a1df5fa114c0a2d01
Rustam Garaev <garaev.rustam.h@gmail.com>
2020-12-20 17:31:01 +0400

    add login feature, main page

--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,23 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .serializers import CreateUserSerializer
 from .models import User
 
 
+class ApiRoot(generics.GenericAPIView):
+    """Main page."""
+    name = 'api-root'
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'companies': '',
+            'customers': '',
+        })
+
+
 class CreateUserApi(generics.CreateAPIView):
     """Register new user."""
+    # TODO: add name
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
