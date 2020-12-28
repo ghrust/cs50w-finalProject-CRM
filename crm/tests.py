6187b5ce2a2c8210402004536e6d4d70f4a2ba43
Rustam Garaev <garaev.rustam.h@gmail.com>
2020-12-28 21:25:21 +0400

    test create user, login

--- crm/tests.py
+++ crm/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+"""Tests for crm app."""
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+
+from . import views
+from .models import User
+
+
+class RegisterUserTestCase(APITestCase):
+    """Test register API."""
+
+    def register_user(self):
+        """Register new test user."""
+
+        url = reverse(views.CreateUserApi.name)
+        data = {
+            'username': 'user01',
+            'email': 'user01@test.com',
+            'password': 'pass',
+            'first_name': 'user',
+            'last_name': '01',
+        }
+        response = self.client.post(url, data, format='json')
+        return response
+
+    def test_register_user(self):
+        """Ensure we can register new user."""
+
+        response = self.register_user()
+
+        self.assertEqual(response.status_code, 201)
+
+        User.objects.first()
+        self.assertEqual(response.data['username'], 'user01')
+
+    def test_login_user(self):
+        """Ensure we can login user."""
+
+        self.register_user()
+        response = self.client.login(username='user01', password='pass')
+
+        self.assertEqual(response, True)
