98b50a60242cdbfb71d08e52640f0d4f42ae11ce
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-02-10 06:08:59 +0400

    login page proto work, tested

--- accounts/views.py
+++ accounts/views.py
@@ -9,5 +9,5 @@ from .forms import UserCreationForm
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
