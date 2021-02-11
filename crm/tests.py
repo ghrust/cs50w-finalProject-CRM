47c1298866cdf6655c696ef7f17370e578950fc6
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-02-11 06:15:26 +0400

    dashboard page proto work tested

--- crm/tests.py
+++ crm/tests.py
@@ -1,10 +1,17 @@
+"""crm/tests.py
+CRM App tests.
+"""
 from django.test import TestCase
+from django.shortcuts import reverse
+
+from loguru import logger
 
 
 class IndexPageTestCase(TestCase):
     """Test root URL."""
     def test_index_page_get(self):
         """Test get request for index page."""
-        url = '/'
+        url = reverse('dashboard')
         response = self.client.get(url)
+        logger.info(response.content)
         self.assertEqual(response.status_code, 200)
