f5a819ba7b0154562df0e2cca3261f8254685d33
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-03-06 13:38:43 +0400

    model created

--- crm/models.py
+++ crm/models.py
@@ -30,3 +30,13 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}\nPhone: {self.phone}'
+
+
+class Category(models.Model):
+    """Product category."""
+    name = models.CharField(
+        verbose_name='product category',
+        max_length=100)
+
+    class Meta:
+        order = 'name'
