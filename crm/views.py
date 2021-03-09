c8b1492147b35f203e20988ceee2ca2463f07a14
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-03-09 05:56:32 +0400

    add ProductCreateView

--- crm/views.py
+++ crm/views.py
@@ -6,13 +6,13 @@ from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from .forms import CustomerForm
-from .models import Customer
+from .models import Customer, Product
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
@@ -61,3 +61,10 @@ def customer_create_view(request):
         form = CustomerForm()
 
     return render(request, 'crm/customer_form.html', {'form': form})
+
+
+# Product.
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    """Product create page."""
+    model = Product
+    fields = ['name', 'category', 'price']
